898cb536973e353d4978e8341a9034378797f306
heaven7 <532278976@qq.com>
2023-09-21 18:35:00 +0800

    opt

---
+++ judy-1.0.5/src/h7_config.h
@@ -0,0 +1,20 @@
+#ifndef JUDY_H7_CONFIG_H
+#define JUDY_H7_CONFIG_H
+
+#ifndef bool_t
+#define bool_t int
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FUNCTION
+#define FUNCTION
+#endif
+
+#endif // H7_CONFIG_H
